ed0672cf1d7bec8dcf5d5f2dbff619e3b7e3f1d9
vineelakommoju1517 <vineela.kommoju1517@gmail.com>
2025-07-04 10:57:04 +0530

    convertion changes added

---
+++ 02-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "vineela:: hi likki how are you?"
+echo "likki:: I am fine, how are you?"
+echo "vineela:: I am also fine, thank you!"
+echo "likki:: I am going out now cnn you join with me!"
+echo "vineela:: yeah sure!"
+echo "likki:: you know? where we are going now?!"
+echo "vineela:: no!"
+echo "likki:: at leaset guess!"
+echo "vineela:: movie?!"
