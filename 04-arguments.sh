01bdb427aa57c5b27b7b01bf022cda8888bda6ff
vineelakommoju1517 <vineela.kommoju1517@gmail.com>
2025-07-04 11:18:22 +0530

    convertion changes added

--- 04-arguments.sh
+++ 04-arguments.sh
@@ -1,15 +1,15 @@
 
 #!/bin/bash
 
-perrson1=$1
+person1=$1
 person2=$2
 
-echo "$perrson1:: hi likki how are you?"
-echo "$perrson2:: I am fine, how are you?"
-echo "$perrson1:: I am also fine, thank you!"
-echo "$perrson2:: I am going out now cnn you join with me!"
-echo "$perrson1:: yeah sure!"
-echo "$perrson2:: you know? where we are going now?!"
-echo "$perrson1:: no!"
-echo "$perrson2:: at leaset guess!"
-echo "$perrson1:: movie?!"
+echo "$person1:: hi likki how are you?"
+echo "$person2:: I am fine, how are you?"
+echo "$person1:: I am also fine, thank you!"
+echo "$person2:: I am going out now cnn you join with me!"
+echo "$person1:: yeah sure!"
+echo "$person2:: you know? where we are going now?!"
+echo "$person1:: no!"
+echo "$person2:: at leaset guess!"
+echo "$person1:: movie?!"
