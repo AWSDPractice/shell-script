bc7b557101772f579ef5cd51c4c4299d894a4b4c
vineelakommoju1517 <vineela.kommoju1517@gmail.com>
2025-07-04 11:15:32 +0530

    convertion changes added

---
+++ 04-arguments.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+
+perrson1=$1
+person2=$2
+
+echo "$perrson1:: hi likki how are you?"
+echo "$perrson2:: I am fine, how are you?"
+echo "$perrson1:: I am also fine, thank you!"
+echo "$perrson2:: I am going out now cnn you join with me!"
+echo "$perrson1:: yeah sure!"
+echo "$perrson2:: you know? where we are going now?!"
+echo "$perrson1:: no!"
+echo "$perrson2:: at leaset guess!"
+echo "$perrson1:: movie?!"
