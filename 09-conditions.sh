5dbfaf6ff1be14a9a41910082ba2ba374f06d572
vineelakommoju1517 <vineela.kommoju1517@gmail.com>
2025-07-08 11:01:28 +0530

    conditional script added

---
+++ 09-conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+Number=$2
+
+if [ $Number -gt 100 ] 
+then
+    echo "The number is greater than 100"
+elif [ $Number -eq 100 ]
+then
+    echo "The number is equal to 100"
+else
+    echo "The number is less than 100"
+fi
